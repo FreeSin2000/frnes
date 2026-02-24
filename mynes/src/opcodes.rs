0c1b3cb2e113743ec964ab3af9e0edbdb2a7d913
FreeSin2000 <3474145990@qq.com>
2026-02-24 21:51:38 +0800

    Add define_opcodes macro.

--- mynes/src/opcodes.rs
+++ mynes/src/opcodes.rs
@@ -2,6 +2,15 @@ use crate::cpu::AddressingMode;
 use std::collections::HashMap;
 use std::sync::LazyLock;
 
+macro_rules! define_opcodes {
+    ($($code:expr, $mnemonic:expr, $len:expr, $cycles:expr, $mode:ident;)*) => {
+        vec![
+            $(
+                OpCode::new($code, $mnemonic, $len, $cycles, AddressingMode::$mode),
+            )*
+        ]
+    };
+}
 pub struct OpCode {
     pub code: u8,
     pub mnemonic: &'static str,
@@ -29,45 +38,29 @@ impl OpCode {
 }
 
 pub static CPU_OPS_CODES: LazyLock<Vec<OpCode>> = LazyLock::new(|| {
-    vec![
-        OpCode::new(0x00, "BRK", 1, 7, AddressingMode::NoneAddressing),
-        OpCode::new(0xaa, "TAX", 1, 2, AddressingMode::NoneAddressing),
-        OpCode::new(0xe8, "INX", 1, 2, AddressingMode::NoneAddressing),
-        OpCode::new(0xa9, "LDA", 2, 2, AddressingMode::Immediate),
-        OpCode::new(0xa5, "LDA", 2, 3, AddressingMode::ZeroPage),
-        OpCode::new(0xb5, "LDA", 2, 4, AddressingMode::ZeroPage_X),
-        OpCode::new(0xad, "LDA", 3, 4, AddressingMode::Absolute),
-        OpCode::new(
-            0xbd,
-            "LDA",
-            3,
-            4, /*+1 if page crossed*/
-            AddressingMode::Absolute_X,
-        ),
-        OpCode::new(
-            0xb9,
-            "LDA",
-            3,
-            4, /*+1 if page crossed*/
-            AddressingMode::Absolute_Y,
-        ),
-        OpCode::new(0xa1, "LDA", 2, 6, AddressingMode::Indirect_X),
-        OpCode::new(
-            0xb1,
-            "LDA",
-            2,
-            5, /*+1 if page crossed*/
-            AddressingMode::Indirect_Y,
-        ),
+    define_opcodes!(
+        0x00, "BRK", 1, 7, NoneAddressing;
+        0xaa, "TAX", 1, 2, NoneAddressing;
+        0xe8, "INX", 1, 2, NoneAddressing;
+        0xa9, "LDA", 2, 2, Immediate;
+        0xa5, "LDA", 2, 3, ZeroPage;
+        0xb5, "LDA", 2, 4, ZeroPage_X;
+        0xad, "LDA", 3, 4, Absolute;
+        0xbd, "LDA", 3, 4, /*+1 if page crossed*/ Absolute_X;
         
-        OpCode::new(0x85, "STA", 2, 3, AddressingMode::ZeroPage),
-        OpCode::new(0x95, "STA", 2, 4, AddressingMode::ZeroPage_X),
-        OpCode::new(0x8d, "STA", 3, 4, AddressingMode::Absolute),
-        OpCode::new(0x9d, "STA", 3, 5, AddressingMode::Absolute_X),
-        OpCode::new(0x99, "STA", 3, 5, AddressingMode::Absolute_Y),
-        OpCode::new(0x81, "STA", 2, 6, AddressingMode::Indirect_X),
-        OpCode::new(0x91, "STA", 2, 6, AddressingMode::Indirect_Y),
-    ]
+        0xb9, "LDA", 3, 4, /*+1 if page crossed*/ Absolute_Y;
+        0xa1, "LDA", 2, 6, Indirect_X;
+        
+        0xb1, "LDA", 2, 5, /*+1 if page crossed*/ Indirect_Y;
+
+        0x85, "STA", 2, 3, ZeroPage;
+        0x95, "STA", 2, 4, ZeroPage_X;
+        0x8d, "STA", 3, 4, Absolute;
+        0x9d, "STA", 3, 5, Absolute_X;
+        0x99, "STA", 3, 5, Absolute_Y;
+        0x81, "STA", 2, 6, Indirect_X;
+        0x91, "STA", 2, 6, Indirect_Y;
+    )
 });
 
 pub static OPCODES_MAP: LazyLock<HashMap<u8, &'static OpCode>> = LazyLock::new(|| {
